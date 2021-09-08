82033421c576d7e310755abc21836ed22cb49f94
filatov <filatov@msd-spb.ru>
2021-09-08 12:22:15 +0300

    Soap клиент с одним методом - Add
    По гайду https://spring.io/guides/gs/consuming-web-service/

---
+++ src/main/java/com/filatov/calc/service/soap/CalcSoapClient.java
@@ -0,0 +1,18 @@
+package com.filatov.calc.service.soap;
+
+import com.filatov.calc.wsdl.Add;
+import com.filatov.calc.wsdl.AddResponse;
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+public class CalcSoapClient extends WebServiceGatewaySupport {
+    public AddResponse doAdd(int a, int b) {
+        final Add add = new Add();
+        add.setIntA(a);
+        add.setIntB(b);
+        return (AddResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://www.dneonline.com/calculator.asmx", add, new SoapActionCallback(
+                        "http://tempuri.org/Add"
+                ));
+    }
+}
