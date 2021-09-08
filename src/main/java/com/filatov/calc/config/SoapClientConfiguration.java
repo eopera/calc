4cd79db16275e53f25d38a68137fff89400ccf09
filatov <filatov@msd-spb.ru>
2021-09-08 12:21:30 +0300

    Конфигурация soap клиента
    По гайду https://spring.io/guides/gs/consuming-web-service/

---
+++ src/main/java/com/filatov/calc/config/SoapClientConfiguration.java
@@ -0,0 +1,28 @@
+package com.filatov.calc.config;
+
+import com.filatov.calc.service.soap.CalcSoapClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapClientConfiguration {
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // this package must match the package in the <generatePackage> specified in
+        // pom.xml
+        marshaller.setContextPath("com.filatov.calc.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public CalcSoapClient calcSoapClient(Jaxb2Marshaller marshaller) {
+        CalcSoapClient client = new CalcSoapClient();
+        client.setDefaultUri("http://www.dneonline.com/calculator.asmx");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+
+}
