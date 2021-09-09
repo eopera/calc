3b683ff2087978fc6ba0e570bfb21594a53fbc77
filatov <filatov@msd-spb.ru>
2021-09-09 15:57:13 +0300

    OpenAPI описание контроллера

--- src/main/java/com/filatov/calc/rest/CalcResource.java
+++ src/main/java/com/filatov/calc/rest/CalcResource.java
@@ -1,14 +1,34 @@
 package com.filatov.calc.rest;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import com.filatov.calc.model.CalcOperation;
+import com.filatov.calc.service.soap.CalcSoapClient;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/do")
 public class CalcResource {
-    @GetMapping("/add")
-    Integer add(@RequestParam int a, @RequestParam int b) {
-        return a + b;
+    private final CalcSoapClient calcSoapClient;
+
+    public CalcResource(CalcSoapClient calcSoapClient) {
+        this.calcSoapClient = calcSoapClient;
     }
+
+    @Operation(summary = "Execute one of available calculator's operation")
+    @GetMapping("/{operation}/{intA}/{intB}")
+    Integer doOperation(
+            @Parameter(description = "Calculator's operation") @PathVariable CalcOperation operation,
+            @Parameter(description = "left operand") @PathVariable Integer intA,
+            @Parameter(description = "right operand") @PathVariable Integer intB) {
+        return calcSoapClient.doOperation(operation, intA, intB);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public void handleException() {
+
+    }
+
+
 }
